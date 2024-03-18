5a8ceac0284670d9c1203a44bc6a62c22f51de33
Cristian Manolache <cristianmanolache0@gmail.com>
2024-03-18 17:46:13 +0200

    Initial commit

---
+++ src/main/java/com/l2c/business/EComerceBusinessSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.l2c.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComerceBusinessSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComerceBusinessSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l2c/business/EComerceBusinessSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2c.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComerceBusinessSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
