3a7efa17f6c337d094cfc7f25dcabc1bfaaf701c
Cristian Manolache <cristianmanolache0@gmail.com>
2024-04-18 20:12:03 +0300

    add REST API operations: create, getById, delete. Also small changes

--- src/main/java/com/l2c/business/repository/ResinTableRepository.java
+++ src/main/java/com/l2c/business/repository/ResinTableRepository.java
@@ -1,9 +1,13 @@
 package com.l2c.business.repository;
 
+import com.l2c.business.dto.ResinTableDto;
 import com.l2c.business.entity.ResinTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResinTableRepository extends JpaRepository<ResinTable, Integer> {
+
+
 }
--- src/main/java/com/l2c/business/service/ResinTableService.java
+++ src/main/java/com/l2c/business/service/ResinTableService.java
@@ -1,4 +1,11 @@
 package com.l2c.business.service;
 
+import com.l2c.business.dto.ResinTableDto;
+
 public interface ResinTableService {
+    ResinTableDto createResinTable(ResinTableDto resinTableDto);
+
+    ResinTableDto getResinTableById(Integer id);
+
+    void deleteResinTableById(Integer id);
 }
